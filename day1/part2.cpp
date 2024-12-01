702616235b6b3e20c273a821925f80d5684f0940
eklavya <eklavyasood@gmail.com>
2024-12-01 19:52:37 +0530

    day1 part2 done

--- day1/part2.cpp
+++ day1/part2.cpp
@@ -1,36 +1,51 @@
+// part2: count occurences of every list1 element in list2 and calculate a similarity score
 #include <iostream>
 #include <fstream>
 #include <ostream>
 #include <string>
 #include <vector>
 
-#define SIZE 6
+// count occurences of list1 elements in list2
+std::vector<int> countOccurences(std::vector<int>& vec1, std::vector<int>& vec2) {
+  std::vector<int> occurences;
+  size_t n = vec1.size();
+  for (size_t i = 0; i < n; i++) {
+    int count = 0;
+    for (size_t j = 0; j < n; j++) {
+      if (vec1[i] == vec2[j])
+        ++count;
+    }
+    occurences.push_back(count);
+  }
+  return occurences;
+}
 
-void bubbleSort(std::vector<int>& arr) {
-  size_t n = arr.size();
+int calculateScore(std::vector<int> vec1, std::vector<int> occurences) {
+  size_t n = vec1.size();
+  int similarityScore = 0;
+
+  for (size_t i = 0; i < n; i++) {
+    similarityScore += (vec1[i] * occurences[i]);
+  }
+
+  return similarityScore;
+}
+
+// sort the elements of both lists
+void bubbleSort(std::vector<int>& vec) {
+  size_t n = vec.size();
   for (size_t step = 0; step < n - 1; ++step) {
     for (size_t i = 0; i < n - step - 1; ++i) {
-      if (arr[i] > arr[i+1]) {
-        int temp = arr[i];
-        arr[i] = arr[i+1];
-        arr[i+1] = temp;
+      if (vec[i] > vec[i+1]) {
+        int temp = vec[i];
+        vec[i] = vec[i+1];
+        vec[i+1] = temp;
       }
     }
   }
 }
 
-// void compareDifference(int list1[], int list2[], int diff[], int n) {
-void compareDifference(std::vector<int> list1, std::vector<int> list2, std::vector<int>& diff) {
-  size_t n = list1.size();
-  diff.resize(n);
-  for (int i = 0; i < n; i++) {
-    if (list1[i] >= list2[i])
-      diff[i] = list1[i] - list2[i];
-    else 
-      diff[i] = list2[i] - list1[i];
-  } 
-}
-
+// read both lists and create two vector arrays out of them
 bool readLists(const std::string& filename, std::vector<int>& array1, std::vector<int>& array2) {
   std::ifstream inputFile(filename);
   if (!inputFile) {
@@ -52,27 +67,18 @@ int main (int argc, char *argv[]) {
 
   const std::string filename = "inputs1.txt";
 
-  std::vector<int> array1;
-  std::vector<int> array2;
+  std::vector<int> vec1;
+  std::vector<int> vec2;
 
-  if (!readLists(filename, array1, array2)) {
+  if (!readLists(filename, vec1, vec2)) {
     std::cout << "unable to read lists from the file" << std::endl;
     return 1;
   }
 
-  // sorting both the lists
-  bubbleSort(array1);
-  bubbleSort(array2);
+  std::vector<int> occurences = countOccurences(vec1, vec2);
 
-  std::vector<int> diff;
-  compareDifference(array1, array2, diff);
-
-  int32_t total = 0;
-  for (size_t i = 0; i < diff.size(); i++) {
-    total += diff[i];
-  }
-
-  std::cout << std::endl << "Total difference = " << total << std::endl;
+  int similarityScore = calculateScore(vec1, occurences);
+  std::cout << similarityScore << std::endl;
 
   return 0;
 
